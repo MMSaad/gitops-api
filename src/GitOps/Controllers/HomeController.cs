39a0328725cd5b4eefc0717868690f7d4484dd11
Mustafa Saad <m.saad@gama-ksa.com>
2022-11-20 16:44:20 +0300

    Fix Docker file

--- src/GitOps/Controllers/HomeController.cs
+++ src/GitOps/Controllers/HomeController.cs
@@ -3,20 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GitOps.Api.Controllers;
 
-    [Route("api/[controller]")]
-    [ApiController]
-    public class HomeController : ControllerBase
+[Route("api/[controller]")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
     {
-
-        public async Task<IActionResult> Index()
-        {
-            var version = await System.IO.File.ReadAllTextAsync("VERSION");
-            return Ok(version);
-        }
-
-        //public async Task<IActionResult> Config()
-        //{
-
-        //}
+        var version = await System.IO.File.ReadAllTextAsync("VERSION");
+        return Ok(version);
     }
+
+    //public async Task<IActionResult> Config()
+    //{
+
+    //}
+}
 
