0c6e5416692e203722bb093808b5fcff8bb595a5
Mustafa Saad <m.saad@gama-ksa.com>
2022-11-20 15:06:27 +0300

    Add project files.

---
+++ GitOps/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitOps.Api.Controllers;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        public async Task<IActionResult> Index()
+        {
+
+        }
+
+        public async Task<IActionResult> Config()
+        {
+
+        }
+    }
+
